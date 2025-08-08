7d7ae6bfbdfacb1113f2c6745d855c88d84a9b21
saulo-duarte <saulolduarte@outlook.com>
2025-08-08 20:24:32 -0300

    feat: add OrderController

---
+++ src/main/java/com/btgpactual/orderms/dtos/ApiResponse.java
@@ -0,0 +1,9 @@
+package com.btgpactual.orderms.dtos;
+
+import java.util.List;
+import java.util.Map;
+
+public record ApiResponse<T>(Map<String, Object> summary,
+                             List<T> data,
+                             PaginationResponse pagination){
+}
