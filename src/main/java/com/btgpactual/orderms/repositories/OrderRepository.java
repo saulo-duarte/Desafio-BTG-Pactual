b04ab3ec25f253840cbd0c25c8b54d0f95437d39
saulo-duarte <saulolduarte@outlook.com>
2025-08-08 20:22:09 -0300

    feat: add OrderRepository

---
+++ src/main/java/com/btgpactual/orderms/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.btgpactual.orderms.repositories;
+
+import com.btgpactual.orderms.entities.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface OrderRepository extends MongoRepository<OrderEntity, Long> {
+
+    Page<OrderEntity> findAllByCustomerId(Long customerId, PageRequest pageRequest);
+}
