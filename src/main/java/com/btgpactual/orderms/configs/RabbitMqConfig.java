f1fade060662b6b7c90456c894960e917197c6e4
saulo-duarte <saulolduarte@outlook.com>
2025-08-08 20:20:02 -0300

    feat: add RabbitMQ configuration

---
+++ src/main/java/com/btgpactual/orderms/configs/RabbitMqConfig.java
@@ -0,0 +1,16 @@
+package com.btgpactual.orderms.configs;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
